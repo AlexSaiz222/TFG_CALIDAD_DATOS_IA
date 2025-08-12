2a2adb6e59f84c87c118740b60db437f39ab4442
Alejandro <alejandro.saiz@alu.uclm.es>
2025-08-13 00:05:37 +0200

    gitignore y config nextjs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   output: 'standalone',
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://backend:5000/api/:path*',
+        destination: process.env.NODE_ENV === 'development' 
+          ? 'http://localhost:5000/api/:path*' 
+          : 'http://backend:5000/api/:path*',
       },
     ];
   },
